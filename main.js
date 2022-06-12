7681239694cc28e9680d7432e38a9fdc6d9553ed
Masaki Oyata <masaki.oyata@gmail.com>
2022-06-12 01:32:17 -0700

    DOM Updating: Adding main css and js file

---
+++ main.js
@@ -0,0 +1,132 @@
+    // Your JavaScript goes here!
+    let playerWins = 0;
+    let computerWins = 0;
+
+    const container = document.querySelector('#container');
+    const result = document.querySelector('#result');
+    const gameState = document.querySelector('#gameState');
+    const playerChoice = document.querySelector('#playerChoice');
+    const computerChoice = document.querySelector('#computerChoice');
+    const gameTally = document.querySelector('#gameTally');
+
+    // Create Buttons for Rock, Paper, Scissors
+    let rockBtn = document.createElement("button"); 
+    rockBtn.classList.add('rockBtn');
+    rockBtn.textContent = 'ROCK';
+
+    let paperBtn = document.createElement("button");
+    paperBtn.classList.add('paperBtn');
+    paperBtn.textContent = "PAPER";
+
+    let scissorsBtn = document.createElement("button");
+    scissorsBtn.classList.add('scissorsBtn');
+    scissorsBtn.textContent = "SCISSORS";
+    
+    container.appendChild(rockBtn);
+    container.appendChild(paperBtn);
+    container.appendChild(scissorsBtn);
+
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.disabled = true;
+        // button.addEventListener('click', function (e) {
+        //     result.textContent = playRound(e.target.textContent, computerPlay());
+        // });
+    });
+
+    function startGame()
+    {
+        playerWins = 0;
+        computerWins = 0;
+
+        gameTally.textContent = `PLAYER SCORE: ${playerWins} -VS- COMPUTER SCORE: ${computerWins}`;
+        playerChoice.textContent = 'Player: ' + 'NONE';
+        computerChoice.textContent = 'Computer: ' + 'NONE';
+        result.textContent = 'Choose ROCK, PAPER, or SCISSORS';
+
+        buttons.forEach((button) => {
+            button.disabled = false;
+        });
+    }
+
+    function computerPlay()
+    {
+        let computerSelection = "";
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+        if(randomNumber <= 33)
+        {
+            computerSelection = 'ROCK';
+        }
+        else if(randomNumber > 33 && randomNumber <= 66)
+        {
+            computerSelection = 'PAPER';
+        }
+        else
+        {
+            computerSelection = 'SCISSORS';
+        }
+
+        return computerSelection;
+    }
+
+    function playRound(playerSelection, computerSelection) {
+        // your code here!
+        playerChoice.textContent = 'Player: ' + playerSelection;
+        computerChoice.textContent = 'Computer: ' + computerSelection;
+
+        gameState.appendChild(playerChoice);
+        gameState.appendChild(computerChoice);
+
+        if(playerSelection == computerSelection)
+        {
+            return 'You Tied!';
+        }
+        else
+        {
+            if(playerSelection == 'ROCK')
+            {
+                if(computerSelection == 'PAPER')
+                {
+                    computerWins++;
+                    gameTally.textContent = `PLAYER SCORE: ${playerWins} -VS- COMPUTER SCORE: ${computerWins}`;
+                    return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+                }
+                else if(computerSelection == 'SCISSORS')
+                {
+                    playerWins++;
+                    gameTally.textContent = `PLAYER SCORE: ${playerWins} -VS- COMPUTER SCORE: ${computerWins}`;
+                    return `You Win! ${playerSelection} beats ${computerSelection}!`;
+                }
+            }
+            if(playerSelection == 'PAPER')
+            {
+                if(computerSelection == 'SCISSORS')
+                {
+                    computerWins++;
+                    gameTally.textContent = `PLAYER SCORE: ${playerWins} -VS- COMPUTER SCORE: ${computerWins}`;
+                    return `You Lost! ${computerSelection} beats ${playerSelection}!`;
+                }
+                else if(computerSelection == 'ROCK')
+                {
+                    playerWins++;
+                    gameTally.textContent = `PLAYER SCORE: ${playerWins} -VS- COMPUTER SCORE: ${computerWins}`;
+                    return `You Won! ${playerSelection} beats ${computerSelection}!`;
+                }
+            }
+            if(playerSelection == 'SCISSORS')
+            {
+                if(computerSelection == 'ROCK')
+                {
+                    computerWins++;
+                    gameTally.textContent = `PLAYER SCORE: ${playerWins} -VS- COMPUTER SCORE: ${computerWins}`;
+                    return `You Lost! ${computerSelection} beats ${playerSelection}!`;
+                }
+                else if(computerSelection == 'PAPER')
+                {
+                    playerWins++;
+                    gameTally.textContent = `PLAYER SCORE: ${playerWins} -VS- COMPUTER SCORE: ${computerWins}`;
+                    return `You Won! ${playerSelection} beats ${computerSelection}!`;
+                }
+            }
+        }
+    }
